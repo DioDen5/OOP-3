04552e0916590024407613274be2cd673da5f57d
DenZastavnyi <denyszastavniy@gmail.com>
2025-05-10 18:24:00 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "Dyhotomia_class.h"
+#include "Newton_class.h"
+
+int main() {
+    Dyhotomia_class d;
+    d.setVolumes(2, 4);
+    d.setTolerance(1e-6);
+    double root_d = d.dyhotomiaMethod();
+    cout << "Корінь методом дихотомії: " << root_d << endl;
+
+    Newton_class n;
+    n.setInitialGuess(3.0);
+    n.setTolerance(1e-6);
+    double root_n = n.newtonMethod();
+    cout << "Корінь методом Ньютона: " << root_n << endl;
+
+    return 0;
+}
